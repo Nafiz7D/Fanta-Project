331ccf95ffb47302ca19ecb566005d0b014e1bdf
Nafiz7D <mrnafsu07@gmail.com>
2024-10-13 13:16:37 +0600

    added footer section

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Banner from "./components/Banner/Banner"
 import BannerText from "./components/Banner/BannerText"
 import Blogs from "./components/Blogs/Blogs"
 import FAQ from "./components/FAQ/FAQ"
+import Footer from "./components/Footer/Footer"
 
 
 
@@ -24,6 +25,7 @@ const App = () => {
         <BannerText></BannerText>
         <Blogs></Blogs>
         <FAQ></FAQ>
+        <Footer></Footer>
       </UpdateFollower>
     </main>
   )
